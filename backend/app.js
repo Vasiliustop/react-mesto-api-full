f7cb37082261dcb9578102b7cfaf9eb2c687efe8
Vasya <vasilgor@rambler.ru>
2022-06-28 18:17:19 +0300

    1try

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,11 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const app = express();
-
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger);
