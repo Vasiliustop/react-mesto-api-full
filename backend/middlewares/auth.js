1952a8954a25ecb8266db405f9e604f9a869fb4d
Vasya <vasilgor@rambler.ru>
2022-07-03 01:09:45 +0300

    1try

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,23 +1,25 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
-// eslint-disable-next-line consistent-return
-module.exports = (req, res, next) => {
-  const { cookies } = req;
 
-  if (!cookies) {
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, __, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthError('Необходима авторизация');
   }
 
-  const token = cookies.jwt;
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new AuthError('Необходима авторизация');
   }
 
-  req.user = payload; // записываем пейлоуд в объект запроса
+  req.user = payload;
 
-  next(); // пропускаем запрос дальше
+  return next();
 };
