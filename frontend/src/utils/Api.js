ae7d05593d699c3d8a8749ba10a2d431c98655f5
Vasya <vasilgor@rambler.ru>
2022-07-06 18:46:06 +0300

    2try

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -4,6 +4,13 @@ export default class Api {
     this._baseUrl = baseUrl;
   }
 
+  _getHeaders() {
+    const token = localStorage.getItem('jwt');
+    return {
+      'Authorization': `Bearer ${token}`,
+      ...this._headers,
+    };
+  }
 
   _checkRequest(response) {
     if (response.ok) {
@@ -15,14 +22,14 @@ export default class Api {
 
   getProfile() {
     return fetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers,
+      headers: this._getHeaders,
       }).then(this._checkRequest);
   }
 
   editProfile(name, about) {
     return fetch(`${this._baseUrl}/users/me`, {
       method: "PATCH",
-      headers: this._headers,
+      headers: this._getHeaders,
       body: JSON.stringify({
         name,
         about,
@@ -32,35 +39,35 @@ export default class Api {
 
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
+      headers: this._getHeaders,
      }).then(this._checkRequest);
   }
 
   addLike(id) {
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: "PUT",
-      headers: this._headers,
+      headers: this._getHeaders,
      }).then(this._checkRequest);
   }
 
   unlike(id) {
     return fetch(`${this._baseUrl}/cards/${id}/likes`, {
       method: "DELETE",
-      headers: this._headers,
+      headers: this._getHeaders,
     }).then(this._checkRequest);
   }
 
   deleteCard(id) {
     return fetch(`${this._baseUrl}/cards/${id}`, {
       method: "DELETE",
-      headers: this._headers,
+      headers: this._getHeaders,
      }).then(this._checkRequest);
   }
 
   addCard(name, link) {
     return fetch(`${this._baseUrl}/cards`, {
       method: "POST",
-      headers: this._headers,
+      headers: this._getHeaders,
       body: JSON.stringify({
         name,
         link,
@@ -72,7 +79,7 @@ export default class Api {
     //редактирование аватара
     return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: "PATCH",
-      headers: this._headers,
+      headers: this._getHeaders,
       body: JSON.stringify(avatar),
     }).then(this._checkRequest);
   }
@@ -81,7 +88,6 @@ export default class Api {
 export const api = new Api({ 
   baseUrl: "https://api.vasilius.students.nomoreparties.sbs",
   headers: {
-    authorization: `Bearer ${localStorage.getItem('jwt')}`,
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json'
   },
 });
